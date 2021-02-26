c306b1651f0da9b3b660f84cf4a714ad2610f0d8
JorbeJr <50496289+Jorbee1234@users.noreply.github.com>
2021-02-26 20:55:31 -0300

    Add files via upload

---
+++ Tabuada.java
@@ -0,0 +1,11 @@
+
+public class Tabuada {
+/* Nome : Jorbe Muniz Braga Junior - Matricula : 0050016533 */
+		public static void main(String[] args) {
+			int tabuada = 10;
+			System.out.println("Tabuada de "+ tabuada);
+		for(int x = 1; x<=10; x++) {
+			System.out.println(x + "x" + tabuada + "=" + x * tabuada);
+		}
+	}
+}
